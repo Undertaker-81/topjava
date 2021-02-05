e520877fe2e18f397f4723dff4e0f52fb5fbfffa
Dmitriy Panfilov <undertaker-81@mail.ru>
2021-02-05 19:42:20 +0500

    import from csv

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE USERS (
                        registered       TIMESTAMP DEFAULT now() NOT NULL,
                        enabled          BOOLEAN   DEFAULT TRUE  NOT NULL,
                        calories_per_day INTEGER   DEFAULT 2000  NOT NULL
-)AS SELECT * FROM CSVREAD('c:/project/topjava21_boot/users.csv');
+)AS SELECT * FROM CSVREAD('classpath:users.csv');
 
 CREATE UNIQUE INDEX users_unique_email_idx
     ON USERS (email);
@@ -18,11 +18,11 @@ CREATE TABLE MEALS (
                        date_time   TIMESTAMP NOT NULL,
                        description VARCHAR(255)NOT NULL,
                        calories    INT       NOT NULL
-) AS SELECT * FROM CSVREAD('c:/project/topjava21_boot/meals.csv');
+) AS SELECT * FROM CSVREAD('classpath:meals.csv');
 CREATE TABLE USER_ROLES (
                             user_id   INT   auto_increment ,
                             role    VARCHAR
-) AS SELECT * FROM CSVREAD('c:/project/topjava21_boot/role.csv');
+) AS SELECT * FROM CSVREAD('classpath:role.csv');
 
 CREATE UNIQUE INDEX meals_unique_user_datetime_idx
     ON meals (user_id, date_time)
